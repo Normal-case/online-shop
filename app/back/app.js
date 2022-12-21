09a48e0b772d7260f2661b0d50ac0a9c7d0cc1fe
Normal-case <8282chan94@gmail.com>
2022-12-21 20:05:52 +0900

    setting bodyParser and login post API

--- app/back/app.js
+++ app/back/app.js
@@ -1,6 +1,17 @@
+// module
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
+
 const PORT = 8000
+const app = express()
+
+// middleware
+app.use(bodyParser.json())
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    return res.json(req.body)
+})
 
 app.listen(PORT, () => {
     console.log(`Express server on ${PORT}`)
