3ff7c8bba729f66cdf6793ad1c51321596c9ac5f
Normal-case <8282chan94@gmail.com>
2023-01-02 19:01:21 +0900

    register form validation at client

--- app/front/pages/user/register.tsx
+++ app/front/pages/user/register.tsx
@@ -4,26 +4,72 @@ import API from '../../api-server'
 
 export default function Register() {
 
-    const [pswordMsg, setPswordMsg] = useState("")
+    const [errorMsg, setErrorMsg] = useState("")
 
     const sendRegisterForm = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         const formData = e.target as HTMLFormElement
+        const body: Object = {
+            name: formData.name.value,
+            username: formData.username.value,
+            password: formData.password.value
+
+        }
+        if (formErrorHandler(formData)) {
+            API.registerPost(body).then(res => handleGetResponse(res))
+        }
+    }
+
+    const handleGetResponse = (res: Object) => {
+        if(res.data.success) {
+            alert('회원가입이 완료되었습니다.')
+            location.href = '/user/login'
+        } else {
+            setErrorMsg(res.data.msg)
+        }
+    }
+
+    const formErrorHandler = (formData: HTMLFormElement) => {
+        const name = formData.name.value
+        const username = formData.username.value
         const psword = formData.password.value
         const pswordConfirm = formData.passwordConfirm.value
-        if(psword !== pswordConfirm) {
-            setPswordMsg("비밀번호가 일치하지 않습니다.")
-        } else {
-            setPswordMsg("")
-            const body: Object = {
-                name: formData.name.value,
-                username: formData.username.value,
-                password: formData.password.value
-    
-            }
-            API.registerPost(body)
-                .then(console.log)
+
+        if (!name || !username) {
+            setErrorMsg("이름 혹은 아이디를 입력해주세요")
+            return false
         }
+
+        if (passwordPolicy(psword, pswordConfirm)) {
+            setErrorMsg("")
+            return true
+        } else {
+            return false
+        }
+
+    }
+
+    const passwordPolicy = (psword: String, pswordConfirm: String) => {
+        // password is required
+        if (!psword || !pswordConfirm) {
+            setErrorMsg('비밀번호를 입력해주세요')
+            return false
+        }
+
+        // password should include number and alphabet. 8 <= length <= 20
+        var includePs = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/
+        if (!includePs.test(psword)) {
+            setErrorMsg("비밀번호 정책을 확인해주세요")
+            return false
+        }
+
+        // password and passwordConfirm should be same
+        if (psword !== pswordConfirm) {
+            setErrorMsg("비밀번호가 일치하지 않습니다")
+            return false
+        }
+
+        return true
     }
 
     return (
@@ -35,7 +81,7 @@ export default function Register() {
                         <input type="text" placeholder="username" name="username" />
                         <input type="password" placeholder="password" name="password" />
                         <input type="password" placeholder="password confirm" name="passwordConfirm" id="" />
-                        <p className="error-message">{pswordMsg}</p>
+                        <p className="error-message">{errorMsg}</p>
                         <button>회원가입</button>
                         <p className='message'>이미 회원이신가요? <Link href="/user/login" legacyBehavior><a>로그인하기</a></Link></p>
                     </form>
