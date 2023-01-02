d9ac5901f32899cbf8f6738d1225f17364753e2b
Normal-case <8282chan94@gmail.com>
2023-01-02 19:46:37 +0900

    install bcrypt and store encrypted password in database (server)

--- app/back/models/UserStorage.js
+++ app/back/models/UserStorage.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+const rounds = 10
 const dbo = require('../bin/db/connect')
 
 class UserStorage {
@@ -13,7 +15,13 @@ class UserStorage {
         if(user) {
             return { success: false, msg: "username is already exist" }
         } else {
-            dbConnect.collection('user').insertOne(userInfo)
+            const encryptedPs = bcrypt.hashSync(userInfo.password, rounds)
+            const body = {
+                name: userInfo.name,
+                username: userInfo.username,
+                password: encryptedPs
+            }
+            dbConnect.collection('user').insertOne(body)
             return { success: true }
         }
     }
