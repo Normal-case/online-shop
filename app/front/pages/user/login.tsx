c3950716c71b6886a273dfbf37f9fbcd1e7fce6a
Normal-case <8282chan94@gmail.com>
2023-01-18 19:40:30 +0900

    create social login button

--- app/front/pages/user/login.tsx
+++ app/front/pages/user/login.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
 
 import API from '../../api-server'
 import { setToken, tokenCheck } from '../../module/Token'
@@ -41,13 +42,23 @@ export default function Login() {
     return (
         <div className="user">
             <div className="user_option_container">
+                {/* 소셜 로그인을 하는 공간 */}
                 <div className="user_option_register">
                     <div className="user_option_register_contents">
-                        <h2 className="user_register_title">아직 회원이 아니신가요?</h2>
+                        <div className='social_login_button'>
+                            <GoogleOutlined /> &nbsp;
+                            <span>구글 아이디로 로그인</span>
+                        </div>
+                        <div className='social_login_button'>
+                            <FacebookOutlined /> &nbsp;
+                            <span>페이스북 아이디로 로그인</span>
+                        </div>
+                        {/* <h2 className="user_register_title">아직 회원이 아니신가요?</h2>
                         <p className="user_register_description">회원가입 후 다양한 이벤트를 체험해보세요. 많은 혜택을 통해 저렴한 가격으로 예쁜 옷을 구매할 수 있습니다.</p>
-                        <button className="user_register_button">회원가입</button>
+                        <button className="user_register_button">회원가입</button> */}
                     </div>
                 </div>
+                {/* 아이디와 비밀번호를 입력하는 div */}
                 <div className="user_login_form">
                     <div className="user_login_form_container">
                         <h2 className="login_form_title">LOGIN</h2>
