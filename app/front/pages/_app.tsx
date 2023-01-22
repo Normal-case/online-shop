60c82ad72e4bd0cbd59f64ac3823b1294bee39ee
Normal-case <8282chan94@gmail.com>
2023-01-22 19:36:44 +0900

    social login on client use next-auth

--- app/front/pages/_app.tsx
+++ app/front/pages/_app.tsx
@@ -2,7 +2,15 @@ import '../styles/globals.css'
 import '../styles/login.css'
 import '../styles/register.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
