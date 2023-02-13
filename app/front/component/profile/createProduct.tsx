505cb5dc3ddccd19afefefa519bf5edbe10927bf
Normal-case <8282chan94@gmail.com>
2023-02-13 17:58:44 +0900

    create product active or not

--- app/front/component/profile/createProduct.tsx
+++ app/front/component/profile/createProduct.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from '../../styles/component/CreateProduct.module.css'
 
@@ -14,23 +14,78 @@ export default function CreateProduct() {
         { value: 'skirt', name: '스커트' },
         { value: 'pants', name: '팬츠' },
         { value: 'underwear', name: '언더웨어' },
-
     ]
+
+    const [submitActive, setSubmitActive] = useState(false)
+    const [productName, setProductName] = useState()
+    const [productCategory, setProductCategory] = useState('category')
+    const [productPrice, setProductPrice] = useState()
+    const [productDesc, setProductDesc] = useState()
+    
+    const onSubmit = () => {
+        if(!submitActive) return
+
+        console.log(productName)
+    }
+
+    const changeProductName = (e: any) => {
+        const value = e.target.value
+        setProductName(value)
+
+        if(value && productPrice && productDesc && productCategory !== 'category') {
+            setSubmitActive(true)
+        } else {
+            setSubmitActive(false)
+        }
+    }
+
+    const changeCategory = (e: any) => {
+        const value = e.target.value
+        setProductCategory(value)
+
+        if(productName && productPrice && productDesc && value !== 'category') {
+            setSubmitActive(true)
+        } else {
+            setSubmitActive(false)
+        }
+    }
+
+    const changePrice = (e: any) => {
+        const value = e.target.value
+        setProductPrice(value)
+
+        if(productName && value && productDesc && productCategory !== 'category') {
+            setSubmitActive(true)
+        } else {
+            setSubmitActive(false)
+        }
+    }
+
+    const changeDesc = (e: any) => {
+        const value = e.target.value
+        setProductDesc(value)
+
+        if(productName && productPrice && value && productCategory !== 'category') {
+            setSubmitActive(true)
+        } else {
+            setSubmitActive(false)
+        }
+    }
 
     return (
         <div className={styles.body}>
             <h2>상품등록</h2>
 
-            <input className={styles.inputText} type='text' placeholder='상품명' /> <br />
-            <select className={styles.options}>
+            <input className={styles.inputText} type='text' placeholder='상품명' onChange={changeProductName} /> <br />
+            <select className={styles.options} onChange={changeCategory}>
                 { selectOptions.map((option) => (
                     <option value={option.value}>
                         {option.name}
                     </option>
                 ))}
-            </select> <input className={styles.inputPrice} type='number' placeholder='가격' />
+            </select> <input className={styles.inputPrice} type='number' placeholder='가격' onChange={changePrice} />
             <br />
-            <textarea className={styles.inputText} placeholder='상품설명' />
+            <textarea className={styles.inputText} placeholder='상품설명' onChange={changeDesc} />
 
             <div>
                 <label htmlFor='input-product-image'>
@@ -46,7 +101,7 @@ export default function CreateProduct() {
                 />
             </div>
 
-            <button className={styles.submit}>상품 등록</button>
+            <button className={submitActive ? styles.submit : styles.submitDisabled} onClick={onSubmit}>상품 등록</button>
         </div>
     )
 }
