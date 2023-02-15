a2b771df59b6dbec54995938f5ea5042bc235339
Normal-case <8282chan94@gmail.com>
2023-02-15 19:56:45 +0900

    index change

--- app/back/app.js
+++ app/back/app.js
@@ -26,7 +26,6 @@ const corsOption = {
 const route = require('./routes')
 
 // middleware
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors(corsOption))
 app.use(express.static('files'))
